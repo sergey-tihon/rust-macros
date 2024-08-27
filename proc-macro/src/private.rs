cc329733106c6d4a00d36d1ef596c2f475cee860
Sergey Tihon <sergey.tihon@gmail.com>
2024-08-27 08:23:52 +0200

    methods generation for private macro

--- proc-macro/src/private.rs
+++ proc-macro/src/private.rs
@@ -1,16 +1,47 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::__private::{Span, TokenStream2};
+use syn::{
+    parse_macro_input, Data::Struct, DataStruct, DeriveInput, Fields::Named, FieldsNamed, Ident,
+};
+
+fn generate_methods(ast: &DeriveInput) -> Vec<TokenStream2> {
+    let named_fields = match ast.data {
+        Struct(DataStruct {
+            fields: Named(FieldsNamed { ref named, .. }),
+            ..
+        }) => named,
+        _ => panic!("Only structs are supported"),
+    };
+
+    named_fields
+        .iter()
+        .map(|field| {
+            let field_name = field.ident.as_ref().unwrap();
+            let field_type = &field.ty;
+            let method_name = Ident::new(&format!("get_{}", field_name), Span::call_site());
+
+            quote! {
+                pub fn #method_name(&self) -> &#field_type {
+                    &self.#field_name
+                }
+            }
+        })
+        .collect()
+}
 
 pub fn main(item: TokenStream) -> TokenStream {
     let items_as_stream: quote::__private::TokenStream = item.clone().into();
     let ast = parse_macro_input!(item as DeriveInput);
-    let name = ast.ident;
+    let name = &ast.ident;
+    let methods = generate_methods(&ast);
 
     quote! {
         #items_as_stream
 
-        impl #name {}
+        impl #name {
+            #(#methods)*
+        }
     }
     .into()
 }
