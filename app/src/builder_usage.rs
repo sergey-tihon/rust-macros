780e9a7fb1a7cae5e5b45c46b10709b1e2c6333e
Sergey Tihon <sergey.tihon@gmail.com>
2024-09-11 10:50:08 +0200

    feat: tests for builder macro

--- app/src/builder_usage.rs
+++ app/src/builder_usage.rs
@@ -1,6 +1,26 @@
-use builder_macro::Builder;
-
-#[derive(Builder)]
-struct Gleipnir {}
-
 pub fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use builder_macro::Builder;
+
+    #[test]
+    fn should_generate_builder_for_struct_with_no_properties() {
+        #[derive(Builder)]
+        struct ExampleStructNoFields {}
+
+        let _: ExampleStructNoFields = ExampleStructNoFields::builder().build();
+    }
+
+    #[test]
+    fn should_generate_builder_for_struct_with_single_property() {
+        #[derive(Builder)]
+        struct Gleipnir {
+            root_of: String,
+        }
+
+        let glr = Gleipnir::builder().root_of("mountain".to_string()).build();
+
+        assert_eq!(glr.root_of, "mountain".to_string());
+    }
+}
